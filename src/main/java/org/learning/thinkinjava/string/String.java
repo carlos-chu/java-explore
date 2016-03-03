f5b74c37c7ccf8574fdef05f5acc0f41e0589ef8
小强鱼 <361497394@qq.com>
2016-03-03 17:00:13 +0800

    init

---
+++ src/main/java/org/learning/thinkinjava/string/String.java
@@ -0,0 +1,12 @@
+package org.learning.thinkinjava.string;
+
+/**
+ * @ClassName: String
+ * @Description: java中String类的方法注意
+ * @author carlos.chu
+ * @date 2016年3月3日 下午3:17:17
+ *
+ */
+public class String {
+
+}
