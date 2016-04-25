459a3ebf4f3998e28cd958fcc54e6553751015f5
小强鱼 <361497394@qq.com>
2016-04-25 10:58:32 +0800

    。。

--- src/main/java/org/learning/algorithm/sort/QuickSort.java
+++ src/main/java/org/learning/algorithm/sort/QuickSort.java
@@ -9,8 +9,10 @@ package org.learning.algorithm.sort;
 public class QuickSort {
 
     public static void main(String[] args) {
-        int a[] = { 49, 38, 65, 97, 76, 13, 27, 49, 78, 34, 12, 64, 5, 4, 62, 99, 98, 54, 56, 17, 18, 23, 34, 15, 35,
-                25, 53, 51 };
+        // int a[] = { 49, 38, 65, 97, 76, 13, 27, 49, 78, 34, 12, 64, 5, 4, 62, 99, 98, 54, 56, 17, 18, 23, 34, 15, 35,
+        // 25, 53, 51 };
+        int a[] = { 49, 38, 65, 10};
+
         quick(a);
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i] + " ");
