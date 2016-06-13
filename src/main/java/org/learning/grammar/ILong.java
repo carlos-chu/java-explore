57bb7f83098ba64e83a734d01ef5f555ba55f88d
小强鱼 <361497394@qq.com>
2016-06-13 10:02:53 +0800

    long

---
+++ src/main/java/org/learning/grammar/ILong.java
@@ -0,0 +1,18 @@
+package org.learning.grammar;
+
+/**
+ * @ClassName: ILong
+ * @Description: long类型注意点
+ * @author carlos.chu
+ * @date 2016年6月8日 下午2:37:40
+ *
+ */
+public class ILong {
+
+    /**
+     * 大写的声明方式是会进行Long.valueOf(long l)的方法的，这样会在-128-127之间有个选择，这是
+     * LongCache实现的一部分，对于超过的部分会直接new出来新的Long对象
+     */
+    Long a = 5L;
+
+}
