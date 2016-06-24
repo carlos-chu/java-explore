b9632ee08ffcf71afa0967dd4a624fbe1ede3197
小强鱼 <361497394@qq.com>
2016-06-24 15:45:26 +0800

    死锁

--- src/main/java/org/learning/concurrent/thread/DeadlockRisk.java
+++ src/main/java/org/learning/concurrent/thread/DeadlockRisk.java
@@ -18,7 +18,9 @@ public class DeadlockRisk {
 
     public int read() {
         synchronized (rA) {
+            System.out.println("read():" + Thread.currentThread().getName() + "获取了resourceA 的锁！");
             synchronized (rB) {
+                System.out.println("read():" + Thread.currentThread().getName() + "获取了resourceB 的锁！");
                 return rB.value + rA.value;
             }
         }
@@ -26,10 +28,27 @@ public class DeadlockRisk {
 
     public void write(int a, int b) {
         synchronized (rB) {
+            System.out.println("write():" + Thread.currentThread().getName() + "获取了resourceB 的锁！");
             synchronized (rA) {
+                System.out.println("write():" + Thread.currentThread().getName() + "获取了resourceA 的锁！");
                 rA.value = a;
                 rB.value = b;
             }
+        }
+    }
+
+    public static void main(String[] args) {
+        final DeadlockRisk d = new DeadlockRisk();
+        for (int i = 0; i < 3; i++) {
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    d.write(1, 2);
+                    System.out.println(d.read());
+                }
+            });
+            t.setName("线程" + i);
+            t.start();
         }
     }
 }
