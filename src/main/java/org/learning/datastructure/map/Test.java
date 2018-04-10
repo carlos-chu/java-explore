36c8b9f6d1df7c81492e6c4d34c9c150f7eb4457
chuwenqiang <bjchuwenqiang@corp.netease,com>
2018-04-10 17:54:39 +0800

    add

---
+++ src/main/java/org/learning/datastructure/map/Test.java
@@ -0,0 +1,7 @@
+package org.learning.datastructure.map;
+
+public class Test {
+
+	public static void main(String[] args) {
+	}
+}
--- src/main/java/org/learning/jvm/oom/JVMHeapOOM.java
+++ src/main/java/org/learning/jvm/oom/JVMHeapOOM.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class JVMHeapOOM {
 
     static class OOMObject {
+    	String s = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
     }
 
     /**
